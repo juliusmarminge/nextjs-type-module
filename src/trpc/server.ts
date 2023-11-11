936a3eab2caf51e228061786624cc17aeb051b53
juliusmarminge <julius0216@outlook.com>
2023-11-11 18:17:52 +0100

    simplify link

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,65 +1,30 @@
+import "server-only";
+
 import {
   TRPCClientError,
-  type TRPCLink,
   createTRPCProxyClient,
   loggerLink,
 } from "@trpc/client";
 import { cookies } from "next/headers";
-
-import { appRouter, type AppRouter } from "~/server/api/root";
+import { appRouter } from "~/server/api/root";
 import { transformer } from "./shared";
-import {
-  callProcedure,
-  type AnyRouter,
-  type inferRouterContext,
-} from "@trpc/server";
+import { callProcedure } from "@trpc/server";
 
 import { observable } from "@trpc/server/observable";
 import type { TRPCErrorResponse } from "@trpc/server/rpc";
 import { createInnerTRPCContext } from "~/server/api/trpc";
+import { cache } from "react";
 
-function directLink<TRouter extends AnyRouter>(opts: {
-  router: TRouter;
-  createContext: () => Promise<inferRouterContext<TRouter>>;
-}): TRPCLink<TRouter> {
-  return (runtime) =>
-    ({ op }) =>
-      observable((observer) => {
-        const { path, input, type } = op;
+const createContext = cache(() => {
+  return createInnerTRPCContext({
+    headers: new Headers({
+      cookie: cookies().toString(),
+      "x-trpc-source": "rsc",
+    }),
+  });
+});
 
-        const promise = opts
-          .createContext()
-          .then(async (ctx) => {
-            const procedureResult = await callProcedure({
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              procedures: opts.router._def.procedures,
-              path,
-              rawInput: input,
-              ctx: ctx,
-              type,
-            });
-
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-            return runtime.transformer.serialize(procedureResult);
-          })
-          .catch((cause: TRPCErrorResponse) => {
-            observer.error(TRPCClientError.from(cause));
-          });
-
-        promise
-          .then((data) => {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            const transformedResult = runtime.transformer.deserialize(data);
-            observer.next({ result: { data: transformedResult } });
-            observer.complete();
-          })
-          .catch((cause: TRPCErrorResponse) => {
-            observer.error(TRPCClientError.from(cause));
-          });
-      });
-}
-
-export const api = createTRPCProxyClient<AppRouter>({
+export const api = createTRPCProxyClient<typeof appRouter>({
   transformer,
   links: [
     loggerLink({
@@ -67,16 +32,29 @@ export const api = createTRPCProxyClient<AppRouter>({
         process.env.NODE_ENV === "development" ||
         (op.direction === "down" && op.result instanceof Error),
     }),
-    directLink({
-      createContext: () => {
-        return createInnerTRPCContext({
-          headers: new Headers({
-            cookie: cookies().toString(),
-            "x-trpc-source": "rsc",
-          }),
-        });
-      },
-      router: appRouter,
-    }),
+    /**
+     * Custom RSC link that invokes procedures directly in the server component
+     */
+    () =>
+      ({ op }) =>
+        observable((observer) => {
+          createContext()
+            .then((ctx) => {
+              return callProcedure({
+                procedures: appRouter._def.procedures,
+                path: op.path,
+                rawInput: op.input,
+                ctx,
+                type: op.type,
+              });
+            })
+            .then((data) => {
+              observer.next({ result: { data } });
+              observer.complete();
+            })
+            .catch((cause: TRPCErrorResponse) => {
+              observer.error(TRPCClientError.from(cause));
+            });
+        }),
   ],
 });
